f9add92f9bd9b1f4206958fb0cd63a5e677a84ec
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-07-15 19:27:44 +0500

    'qwerty'

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,13 +24,18 @@ const toggleMenu = () => {
         <div id={'navbar'} className={'w-full h-20 py-5 flex  items-center lg:justify-around justify-between lg:px-0 px-5 fixed bg-white z-50'}>
             <img src={require('../Assets/NavLogo.png')} alt=""/>
             <ul id={'ul'}
-                className={`fixed inset-0 flex flex-col items-center justify-center bg-white z-10 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 md:static md:flex-row md:gap-8 lg:gap-16 md:bg-transparent md:translate-x-0`}>
+                className={`fixed inset-0 flex flex-col items-center justify-center gap-2 bg-white w-[50%] z-10 transform ${isOpen ? 'translate-x-0' : '-translate-x-full' } transition-transform duration-300 md:static md:flex-row md:gap-8 lg:gap-16 md:bg-transparent md:translate-x-0`}>
                 <li><a id={'a'} className={'text-black font-medium'} href="#aboutUs">BIZ HAQIMIZDA</a></li>
                 <li><a id={'a'} className={'text-black font-medium'} href="#service">XIZMATLAR</a></li>
                 <li><a id={'a'} className={'text-black font-medium'} href="#info">SHIFOKORLAR</a></li>
                 <li><a id={'a'} className={'text-black font-medium'} href="#contact">KONTAKTLAR</a></li>
                 <button id={'background'}
                       onClick={showModal}  className={'block md:hidden py-2 px-7 rounded-lg font-medium text-white bg-blue-400'}>Bog’lanish
+                </button>
+                <button className="absolute top-5 right-5 text-black md:hidden focus:outline-none" onClick={toggleMenu}>
+                    <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
                 </button>
             </ul>
             <button id={'background'}
